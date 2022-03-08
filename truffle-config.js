250d2358aa5e279062e1d5e28afc03642066cef4
christophermontero <cgortizm21@gmail.com>
2022-03-08 12:03:46 -0500

    Update the solc version from 0.5.0 to 0.5.17

--- truffle-config.js
+++ truffle-config.js
@@ -2,22 +2,22 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-    networks: {
-        development: {
-            host: 'HTTP://127.0.0.1',
-            port: '7545',
-            netwok_id: '*' // Connect to any network
-        }
-    },
-    contracts_directory: './src/contracts',
-    contracts_build_directory: './src/truffle-abis',
-    compilers: {
-        solc: {
-            version: '^0.5.0',
-            optimizer: {
-                enabled: true,
-                runs: 200
-            }
-        }
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: '7545',
+      network_id: '*' // Connect to any network
     }
-}
+  },
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/truffle-abis',
+  compilers: {
+    solc: {
+      version: '^0.5.0',
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
