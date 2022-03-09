2a4bef16a58bfd85dbb283feabb37df8aa9638b3
christophermontero <cgortizm21@gmail.com>
2022-03-09 12:16:20 -0500

    Add the function to allowance a transfer

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -12,13 +12,14 @@ contract Tether {
         uint _amount
     );
 
-    event Approve(
+    event Approval(
         address indexed _owner,
         address indexed _spender,
         uint _amount
     );
 
-    mapping(address => uint256) balanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
@@ -26,9 +27,31 @@ contract Tether {
 
     function transfer(address _to, uint256 _amount) public returns(bool success) {
         require(balanceOf[msg.sender] >= _amount);
+        
         balanceOf[msg.sender] -= _amount;
         balanceOf[_to] += _amount;
+        
         emit Transfer(msg.sender, _to, _amount);
+        
+        return true;
+    }
+
+    function approve(address _spender, uint256 _amount) public returns(bool success){
+        allowance[msg.sender][_spender] = _amount;
+        
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) public returns(bool success) {
+        require(balanceOf[_from] >= _amount);
+        require(allowance[_from][msg.sender] >= _amount);
+
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+        allowance[msg.sender][_from] -= _amount;
+
+        emit Transfer(_from, _to, _amount);
+
         return true;
     }
 }
