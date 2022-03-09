d2fd0d38d32e3b988f998b95fc145f2c575e14d4
christophermontero <cgortizm21@gmail.com>
2022-03-09 12:42:03 -0500

    Create the Reward token

--- src/contracts/Reward.sol
+++ src/contracts/Reward.sol
@@ -1,8 +1,8 @@
 pragma solidity '0.5.17';
 
-contract Tether {
-    string public name = "Tether";
-    string public symbol = "USDT";
+contract Reward {
+    string public name = "Reward token";
+    string public symbol = "RWD";
     uint256 public totalSupply = 1000000000000000000;
     uint8 public decimals = 18;
 
