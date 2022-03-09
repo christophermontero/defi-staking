ce8e3efe57007c37a36b302c67d4939856230ddc
christophermontero <cgortizm21@gmail.com>
2022-03-09 16:06:45 -0500

    Add transactions between contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,13 @@ const DeBank = artifacts.require('DeBank');
 const Reward = artifacts.require('Reward');
 const Tether = artifacts.require('Tether');
 
-module.exports = async function (deployer) {
-  await deployer.deploy(DeBank);
-  await deployer.deploy(Reward);
-  await deployer.deploy(Tether);
+module.exports = async function (deployer, network, accounts) {
+  const tether = await deployer.deploy(DeBank);
+  const reward = await deployer.deploy(Reward);
+  const debank = await deployer.deploy(Tether);
+
+  await reward.transfer(DeBank.address, 1000000000000000000000000);
+
+  // Distribute 100 USDT to investor
+  await tether.transfer(accounts[1], 1000000000000000000);
 };
--- src/contracts/DeBank.sol
+++ src/contracts/DeBank.sol
@@ -1,6 +1,16 @@
 pragma solidity '0.5.17';
 
+import './Reward';
+import './Tether';
+
 contract DeBank {
     address public owner;
     string public name = "DeBank";
+    Tether public tether;
+    Reward public reward;
+
+    constructor(Reward _reward, Tether _tether) public {
+        tether = _tether;
+        reward = _reward;
+    }
 }
