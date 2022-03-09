013c9ac6331f572ea0baac03b27dbfc0b5a55ccf
christophermontero <cgortizm21@gmail.com>
2022-03-09 12:52:17 -0500

    Ignore the folder with the ABIs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
+const DeBank = artifacts.require('DeBank');
+const Reward = artifacts.require('Reward');
 const Tether = artifacts.require('Tether');
 
 module.exports = async function (deployer) {
+  await deployer.deploy(DeBank);
+  await deployer.deploy(Reward);
   await deployer.deploy(Tether);
 };
---
+++ src/contracts/DeBank.sol
@@ -0,0 +1,6 @@
+pragma solidity '0.5.17';
+
+contract DeBank {
+    address public owner;
+    string public name = "DeBank";
+}
