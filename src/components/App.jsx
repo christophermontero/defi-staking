4a1878defc2fe283802be14d050c47aba66ee02a
christophermontero <cgortizm21@gmail.com>
2022-03-31 11:53:06 -0500

    Add function to load Web3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar'
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Web3 from 'web3';
 
 class App extends Component {
+  async UNSAFE_componentWillMount() {
+    await this.loadedWeb3();
+  }
+
+  async loadedWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('No ethereum browser detected! You can check out Metamask');
+    }
+  }
+
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       account: '0x0'
-    }
+    };
   }
 
   render() {
@@ -14,8 +30,8 @@ class App extends Component {
       <div>
         <Navbar account={this.state.account} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
